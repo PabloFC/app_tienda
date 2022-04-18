0d03bb691ea92ea09f9df328e406113da88e8492
PabloFC <pablofc_68@hotmail.com>
2022-04-18 13:34:59 +0200

    inicie mi repositorio

---
+++ src/componentes/Inicio.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Inicio = () => {
+    return ( 
+        <div>
+            <h1>Inicio</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam minima quidem, aspernatur magnam, magni error minus, soluta natus adipisci optio voluptates. Sunt repudiandae aspernatur illum asperiores distinctio fugit unde recusandae!</p>
+        </div>
+     );
+}
+ 
+export default Inicio;
